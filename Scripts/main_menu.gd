d0419775337201a703442927396089ba80ab2c9c
Juanma <juanma1997@outlook.com>
2026-02-10 16:30:39 +0100

    Update menu
    soy bobo y daba un error de redundancia en el codigo,y bien dado el error

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -21,12 +21,11 @@ func _on_start_pressed() -> void:
 func _on_settings_pressed() -> void:
 	vbox_container.visible = false
 	settings_menu.visible = true
- # Replace with function body.
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
- # Replace with function body.
+ 
 
 func on_exit_settings_menu() -> void:
 	vbox_container.visible = true
@@ -35,8 +34,5 @@ func on_exit_settings_menu() -> void:
 
 
 func handle_connecting_signals() -> void:
-	start_button.pressed.connect(_on_start_pressed)
-	settings_button.pressed.connect(_on_settings_pressed)
-	exit_button.pressed.connect(_on_exit_pressed)
 	settings_menu.exit_settings_menu.connect(on_exit_settings_menu)
 	
